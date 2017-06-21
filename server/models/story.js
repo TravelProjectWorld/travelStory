d47b9411d0ded5256d71202b6003f30910bd76af
Daniela <bizgudaniela1@gmail.com>
2017-06-21 08:53:20 -0700

    Story Form Validations
    Display front end validations on Story Form.
    Display backend validations on Story Form.

--- server/models/story.js
+++ server/models/story.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 const StorySchema=new mongoose.Schema(
   {
     user:  {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    country: {type: String},
-    title: { type: String},
-    description: {type: String},
-    url : {type : String},
+    country: {type: String, required:[true, "Please select a country."], minlength: [1, "Please select a country."]},
+    title: { type: String, required:[true, "Please add a title."], minlength: [4, "Title needs to be at least 4 characters long."], maxlength:[20, "Title can`t be longer than 20 characters."]},
+    description: {type: String, required:[true, "Please add description."], minlength:[10, "Description needs to be at least 10 characters long."], maxlength:[2000, "Description can have more than 2000 characters."]},
+    url : {type : String, required: [true, "Please add image url."]},
   },
   {
     timestamps: true
